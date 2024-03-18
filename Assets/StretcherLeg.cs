12f2faa1668bd440242fc1e956153695ae9ff2d5
LintaRahman <zcablra@ucl.ac.uk>
2024-03-18 16:39:14 +0000

    test

---
+++ Assets/StretcherLeg.cs
@@ -0,0 +1,33 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using Ubiq.Messaging;
+// using Ubiq.Networking;
+
+// public class StretcherLeg : MonoBehaviour,  INetworkObject, INetworkConnection 
+// {
+
+//     public void Dispose()
+//     {
+//         throw new System.NotImplementedException();
+//     }
+
+//     public ReferenceCountedMessage Receive()
+//     {
+//         throw new System.NotImplementedException();
+//     }
+
+//     public void Send(ReferenceCountedMessage m)
+//     {
+//         throw new System.NotImplementedException();
+//     }
+
+//     // Start is called before the first frame update
+  
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//     }
+// }
+
